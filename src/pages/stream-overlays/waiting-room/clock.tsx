ec7b56aea27a4051f93d3e6acd5cd6a3035cd04b
Diablohu <huxin@cmcm.com>
2025-03-07 17:51:56 +0800

    init astro

---
+++ src/pages/stream-overlays/waiting-room/clock.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import dayjs from "dayjs";
+
+// ============================================================================
+
+let interval: ReturnType<typeof setInterval>;
+const getCurrentTimeText = () => dayjs().format("HH:mm:ss");
+
+// ============================================================================
+
+const Clock: React.FC<
+    React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLSpanElement>,
+        HTMLSpanElement
+    >
+> = ({ className }) => {
+    const [text, setText] = useState(getCurrentTimeText());
+
+    useEffect(() => {
+        interval = setInterval(() => {
+            setText(getCurrentTimeText());
+        }, 1_000);
+        return () => {
+            if (interval) clearInterval(interval);
+        };
+    }, []);
+
+    return <span className={className}>{text}</span>;
+};
+
+export default Clock;
