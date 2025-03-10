726f2cce9215b38ee73e1288aebc25d13cb7df2d
Diablohu <huxin@cmcm.com>
2025-03-10 19:15:26 +0800

    update server script

--- server/main.mjs
+++ server/main.mjs
@@ -2,12 +2,14 @@ import fs from "node:fs";
 import path from "node:path";
 import express from "express";
 import fileUrl from "file-url";
-import { MSFS_API } from "msfs-simconnect-api-wrapper";
 import debug from "debug";
+import { MSFS_API } from "msfs-simconnect-api-wrapper";
+import { OBSWebSocket } from "obs-websocket-js";
 
 // Configuration ==============================================================
 
 const port = 4321;
+const retryInterval = 60; // seconds
 
 // ============================================================================
 
@@ -20,38 +22,68 @@ if (!fs.existsSync(astroServerEntryFile)) {
     throw new Error("Astro server entry not found! Run `build` command first!");
 }
 
-debug.enable("*");
-const simconnectDebug = debug("SimConnect");
+debug.enable("OBS,MSFS,AstroServer");
+const obsDebug = debug("OBS");
+const msfsDebug = debug("MSFS");
 const astroServerDebug = debug("AstroServer");
 
-(() => {
-    const api = new MSFS_API();
-    api.connect({
+const obsApp = new OBSWebSocket();
+const msfsApp = new MSFS_API();
+const serverApp = express();
+
+await (async () => {
+    const connect = async () => {
+        try {
+            const obs = await obsApp.connect(
+                "ws://192.168.0.4:4455",
+                "password",
+                {
+                    rpcVersion: 1,
+                }
+            );
+            obsDebug("Connected!", obs);
+        } catch (err) {
+            obsDebug(
+                `Connection failed: retrying in %o seconds. %O`,
+                retryInterval,
+                {
+                    code: err.code,
+                    message: err.message,
+                }
+            );
+            // obsDebug(`Reason: [${err.code}] ${err.message}`);
+            setTimeout(connect, retryInterval * 1000);
+        }
+    };
+    connect();
+})();
+
+await (async () => {
+    msfsApp.connect({
         retries: Infinity,
-        retryInterval: 5,
+        retryInterval,
         onConnect: (...args) => {
-            simconnectDebug("Connected!", ...args);
+            msfsDebug("Connected!", ...args);
         },
         onRetry: (_, interval) => {
-            simconnectDebug(
-                `Connection failed: retrying in ${interval} seconds.`
-            );
+            msfsDebug(`Connection failed: retrying in %o seconds.`, interval);
         },
     });
 })();
 
-const astroServerModule = await import(fileUrl(astroServerEntryFile));
-const app = express();
-// Change this based on your astro.config.mjs, `base` option.
-// They should match. The default value is "/".
-const base = "/";
-app.use(
-    base,
-    express.static(path.resolve(import.meta.dirname, "../dist/client"))
-);
-app.use(astroServerModule.handler);
+await (async () => {
+    const astroServerModule = await import(fileUrl(astroServerEntryFile));
+    // Change this based on your astro.config.mjs, `base` option.
+    // They should match. The default value is "/".
+    const base = "/";
+    serverApp.use(
+        base,
+        express.static(path.resolve(import.meta.dirname, "../dist/client"))
+    );
+    serverApp.use(astroServerModule.handler);
 
-app.listen(port, function (err) {
-    if (err) astroServerDebug("Error in server setup");
-    astroServerDebug("Server listening on Port", port);
-});
+    serverApp.listen(port, function (err) {
+        if (err) astroServerDebug("Error in server setup");
+        astroServerDebug("Server listening on Port %o", port);
+    });
+})();
